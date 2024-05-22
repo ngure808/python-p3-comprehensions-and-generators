131e347cde7cba24ecdb6fbbdce748886d8b3474
ngure808 <mwangingure808@gmail.com>
2024-05-22 10:52:37 +0300

    list comprehensions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    num_list = [i for i in num_list if i % 2 == 0]
+    return (num_list)
     pass
 
 def make_exclamation(sentence_list):
+    modified_list = [sentence + "!" for sentence in sentence_list]
+    return modified_list
     pass
